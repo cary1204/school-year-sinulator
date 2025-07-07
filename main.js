7d2f72309b26837949f1b0e445c7565693c56302
Cary <homrwork.cary@gmail.com>
2025-07-06 21:31:42 -0400

    added phaser 2d engine, updated planning list. the "doc" file was for testing, ignore it, will delete it

--- main.js
+++ main.js
@@ -64,6 +64,15 @@ function create() {
   cursors = this.input.keyboard.createCursorKeys();
   this.physics.add.collider(player, platforms);
 
+  //evennet zone
+  this.eventZone = this.add.zone(600, 400, 100, 100);
+  this.physics.world.enable(this.eventZone);
+  this.eventZone.body.setAllowGravity(false);
+  this.eventZone.body.moves = false;
+
+  this.physics.add.overlap(player, this.eventZone, triggerEvent, null, this);
+
+
   this.cameras.main.startFollow(player);
   this.cameras.main.setBounds(0, 0, 1600, 450);
   this.physics.world.setBounds(0, 0, 1600, 450);
@@ -84,4 +93,52 @@ function update() {
   if (cursors.up.isDown && player.body.touching.down) {
     player.setVelocityY(-400);
   }
+}
+
+let eventTriggered = false;
+
+function triggerEvent() {
+  if (eventTriggered) return;
+  eventTriggered = true;
+  player.setVelocityX(0); // Stop player movement
+
+  // Freeze world
+  game.scene.scenes[0].physics.pause();
+
+  const appDiv = document.createElement('div');
+  appDiv.id = 'eventPopup';
+  appDiv.style.position = 'absolute';
+  appDiv.style.top = '50%';
+  appDiv.style.left = '50%';
+  appDiv.style.transform = 'translate(-50%, -50%)';
+  appDiv.style.background = '#222';
+  appDiv.style.color = '#fff';
+  appDiv.style.padding = '20px';
+  appDiv.style.border = '3px solid white';
+  appDiv.style.zIndex = 1000;
+
+  appDiv.innerHTML = `
+    <h3>A cafeteria fight breaks out!</h3>
+    <button onclick="chooseEvent('Shield with Chromebook')">Use Chromebook</button>
+    <br><br>
+    <button onclick="chooseEvent('Summon Band Kids')">Call Band Kids</button>
+    <br><br>
+    <button onclick="chooseEvent('Cry under the table')">Cry</button>
+  `;
+  document.body.appendChild(appDiv);
+}
+
+function chooseEvent(option) {
+  const resultText = {
+    'Shield with Chromebook': "You blocked a chicken nugget. Your screen shattered.",
+    'Summon Band Kids': "They charged with trombones. You gained +5 respect.",
+    'Cry under the table': "The janitor fist bumps you in solidarity."
+  }[option];
+
+  document.getElementById('eventPopup').innerHTML = `<p>${resultText}</p><br><button onclick="closeEvent()">Continue</button>`;
+}
+
+function closeEvent() {
+  document.getElementById('eventPopup').remove();
+  game.scene.scenes[0].physics.resume();
 }
